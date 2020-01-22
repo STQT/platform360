358d0a7c2859b05d028c6b9ad0dfc19555739c43
Sergey Kulichkin <s.kulichkin@gmail.com>
2020-01-22 13:58:03 +0500

    Updated Yandex Metrika

--- resources/views/layouts/krpano.blade.php
+++ resources/views/layouts/krpano.blade.php
@@ -146,14 +146,14 @@
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(57117031, "init", {
+   ym(52658560, "init", {
         clickmap:true,
         trackLinks:true,
         accurateTrackBounce:true,
         webvisor:true
    });
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/57117031" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/52658560" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 
 {{--
