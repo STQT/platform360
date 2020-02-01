dfbd1d7b1d74d59953b6a49ac69659318e96cff4
Sergey Kulichkin <s.kulichkin@gmail.com>
2020-02-01 18:48:43 +0500

    Changed Google Maps API version

--- resources/views/layouts/krpano.blade.php
+++ resources/views/layouts/krpano.blade.php
@@ -121,7 +121,7 @@
 
 @yield('scripts')
 
-<script type="text/javascript" async="" src="https://maps.google.com/maps/api/js?v=3.30&key=AIzaSyCuukKz6F0pH77lK5jgKdNEHa7AFqPMh4k&callback=initMap"></script>
+<script type="text/javascript" async="" src="https://maps.google.com/maps/api/js?v=quarterly&key=AIzaSyCuukKz6F0pH77lK5jgKdNEHa7AFqPMh4k&callback=initMap"></script>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({
