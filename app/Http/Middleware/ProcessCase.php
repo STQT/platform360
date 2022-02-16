5a81131a2731db3d1f06e0c24e1624a97fc8f0ed
Rustambx <rustambx@gmail.com>
2022-02-16 15:21:28 +0500

    Fixed ProccesCase middleware

--- app/Http/Middleware/ProcessCase.php
+++ app/Http/Middleware/ProcessCase.php
@@ -3,14 +3,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Route;
 
 class ProcessCase {
 
     public function handle($request, Closure $next)
     {
-//        if (preg_match('/[A-Z]/', $request->getRequestUri())) {
-//            return redirect(strtolower($request->getRequestUri()), 301);
-//        }
+        if (!$request->isMethod('post')) {
+            if (preg_match('/[A-Z]/', $request->getRequestUri())) {
+                return redirect(strtolower($request->getRequestUri()), 301);
+            }
+        }
 
         return $next($request);
     }
